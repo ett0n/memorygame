02927cbcfb511750c5515d11a138b28a34d154c5
ett0n <mathieu.notte6@gmail.com>
2022-08-20 17:10:48 +0200

    winscreen & history

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,17 @@ import { startTimer, compare, lifePrint, chrono } from "./functions";
 
 const grid: any = document.querySelector("#grid");
 const gameScreen: any = document.querySelector("#game-screen");
-const loseScreen: any = document.querySelector("#lose-screen");
-const loseInfo: any = document.querySelector("#lose-infos");
+const endScreen: any = document.querySelector("#end-screen");
+const endInfo: any = document.querySelector("#end-infos");
+const leaderboard: any = document.querySelector("#leaderboard");
 
 let board: string[] = [];
 const symbols = ["🍧", "🥨", "🍗", "☕", "🥪", "🍌", "🍔", "🍕"];
 let counterSuccess: number = 0;
 let gameInterval: any;
 let lifesCnt: number = 0;
+let highScores: string = "";
+let tableHead: String = "";
 
 //keep selection step to get dataset id of two elements
 let selectedEl: HTMLElement[] = [];
@@ -17,6 +20,10 @@ let selectedEl: HTMLElement[] = [];
 [...symbols, ...symbols].forEach((s) => (Math.random() > 0.5 ? board.push(s) : board.unshift(s)));
 
 grid.innerHTML = board.map((i, index) => `<div class="hidden" data-id="${index}">${i}</div>`).join("");
+
+printLeader();
+
+console.log(new Date().toLocaleString());
 
 //begin stopwatch on grid click from ./functions.ts
 grid.addEventListener(
@@ -93,11 +100,53 @@ grid.addEventListener("click", (e: any) => {
 function win() {
   clearInterval(gameInterval);
   console.log("gg");
+  gameScreen.classList.toggle("active");
+  endScreen.classList.toggle("active");
+  endInfo.innerHTML = "You won in <strong>" + chrono.textContent + "</strong> s, <br/> and <strong>" + lifesCnt + "</strong> lifes left! <br/> Well done !";
+  addLeader();
 }
 
 function lose() {
   clearInterval(gameInterval);
   gameScreen.classList.toggle("active");
-  loseScreen.classList.toggle("active");
-  loseInfo.innerHTML = "You lost with " + counterSuccess + " correct guessses in " + chrono.textContent + "s";
+  endScreen.classList.toggle("active");
+  endInfo.innerHTML = "You lost with " + counterSuccess + " correct guessses in " + chrono.textContent + "s";
 }
+
+function addLeader() {
+  highScores = highScores.replace(
+    /^/,
+    `<tr>
+  <td>${new Date().toLocaleString()}</td>
+  <td>${lifesCnt}</td>
+  <td>${chrono.textContent}</td>
+</tr>`
+  );
+
+  localStorage.setItem("scores", JSON.stringify(highScores));
+  printLeader();
+}
+
+function printLeader() {
+  let domLeaderBoard: any = leaderboard.querySelector("#leader-inject");
+
+  //check if localstorage exists
+  if (localStorage.getItem("scores") === null) {
+    leaderboard.querySelector(".leaderboard-title").classList.add("domhide");
+    leaderboard.querySelector(".leaderboard-clear").classList.add("domhide");
+  } else {
+    leaderboard.querySelector(".leaderboard-title").classList.remove("domhide");
+    leaderboard.querySelector(".leaderboard-clear").classList.remove("domhide");
+    tableHead = "<th>Attempt Date</th><th>Life</th><th>Time</th>";
+  }
+
+  highScores = localStorage.getItem("scores") !== null ? JSON.parse(localStorage.scores) : "";
+  let stringScore: String = (tableHead += highScores);
+  domLeaderBoard.innerHTML = stringScore;
+}
+
+document.querySelector(".leaderboard-clear")?.addEventListener("click", () => {
+  localStorage.clear();
+  printLeader();
+  leaderboard.querySelector("#leader-inject").innerHTML = "";
+});
